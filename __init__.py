1cdb0a03630a9390e003641fd893e75a1b528d56
hidayat hasan (Dale) <weiwei45@hotmail.com>
2022-06-09 12:06:35 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+import logging
+from datetime import datetime
+from pathlib import Path
+from os import getenv
+
+from dotenv import load_dotenv
+
+
+# setup logging
+logger = logging.getLogger(__name__)
+
